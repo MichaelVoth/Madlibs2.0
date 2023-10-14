3973b0629125d1c899aa746c57e46675f04f675b
Michael <michaeldvoth@me.com>
2023-10-14 13:37:30 -0700

    template class

--- server/classes/template.class.js
+++ server/classes/template.class.js
@@ -4,7 +4,7 @@ class Template {
     constructor(title, body, authorID, tags = [], isFeatured = false) {
         this.title = title;
         this.body = body;
-        this.prompts = this.extractPromptsFromBody(body); 
+        this.prompts = this.extractPromptsFromBody(body);
         this.authorID = authorID;
         this.rating = {
             average: 0,
@@ -15,9 +15,62 @@ class Template {
         this.isFeatured = isFeatured;
     }
 
-    // ... other methods ...
-
-    extractPromptsFromBody(body) {
-        // Logic to extract prompts from the body and return them as an array
+    extractPromptsFromBody = (text) => {
+        const regex = /\{([^}]+)\}/g; // Matches all occurrences of text between curly brackets
+        let match;
+        const prompts = [];
+        while ((match = regex.exec(text)) !== null) {
+            prompts.push(match[1].trim()); // Add the matched prompt (without curly brackets) to the prompts array
+        }
+        return prompts;
     }
+
+    renderMadlib(answers) {
+        let renderedText = this.body;
+        this.prompts.forEach((prompt, index) => {
+            const answer = answers[index] || `{${prompt}}`; // Use the original prompt if no answer is provided
+            renderedText = renderedText.replace(`{${prompt}}`, answer);
+        });
+        return renderedText;
+    }
+    
+    getPromptCount() {
+        return this.prompts.length;
+    }
+
+    validateAnswers(answers) {
+        return answers.length === this.prompts.length;
+    }
+
+    addRating(rating) {
+        const totalRating = this.rating.average * this.rating.count;
+        this.rating.count++;
+        this.rating.average = (totalRating + rating) / this.rating.count;
+    }
+
+    addView() {
+        this.views++;
+    }
+
+    addTags(inputString) {
+        let tags = inputString.split(',');
+        tags = tags.map(tag => tag.trim()).filter(tag => tag !== "");
+        tags = [...new Set(tags)];
+        this.tags.push(...tags);
+    }
+    removeTag(tagToRemove) {
+        this.tags = this.tags.filter(tag => tag !== tagToRemove);
+    }
+    getTags() {
+        return this.tags;
+    }
+        
+
+    updateTemplate(newTitle, newBody) {
+        this.title = newTitle || this.title;
+        this.body = newBody || this.body;
+        this.prompts = this.extractPromptsFromBody(this.body);
+    }
+    
+
 }
